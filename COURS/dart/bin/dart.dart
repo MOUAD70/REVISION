bebada9dd7ade9d8cc545f492c769703bf483b2d
mouad <mouadinfo30@gmail.com>
2026-02-10 18:11:17 +0100

    Learning Dart Basics

---
+++ COURS/dart/bin/dart.dart
@@ -0,0 +1,33 @@
+void main(List<String> arguments) {
+  // Variables:
+
+  // var name = "MOUAD";
+
+  //  A final variable:
+  //  Can only be assigned once
+  //  But the value is decided when the program runs
+  // final age = 25;
+  // age = 28;
+
+  //  A const variable:
+  //  Must be known before the program runs
+  //  Immutable forever
+  //  Stored directly in memory (very fast)
+  // const isOpen = true;
+  // isOpen = false;
+
+  // Math Operations:
+
+  // var name = "MOUAD";
+  // print("My name's $name");
+
+  // const age = 25;
+  // print(age);
+
+  // print(age + 5);
+  // print(age - 5);
+  // print(age * 5);
+  // print(age / 5);
+  // print(age % 5);
+
+} 
